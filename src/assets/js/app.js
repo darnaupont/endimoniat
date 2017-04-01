5c98c2020047c54d3816a5c815872c5bd5daeff3
Damià Arnau Pont <damiarnau@gmail.com>
2017-04-01 04:08:11 +0200

    base funcional- falten fotos i icones

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,21 @@
 $(document).foundation();
+$(document).on('click', '.manacor', function(){
+  $('[name=dimoni] option').filter(function() {
+      return ($(this).text() == 'Manacor'); //To select Blue
+  }).prop('selected', true);
+});
+$(document).on('click', '.arta', function(){
+  $('[name=dimoni] option').filter(function() {
+      return ($(this).text() == 'Artà'); //To select Blue
+  }).prop('selected', true);
+});
+$(document).on('click', '.santllorenc', function(){
+  $('[name=dimoni] option').filter(function() {
+      return ($(this).text() == 'Sant Llorenç des Cardassar'); //To select Blue
+  }).prop('selected', true);
+});
+$(document).on('click', '.algaida', function(){
+  $('[name=dimoni] option').filter(function() {
+      return ($(this).text() == 'Algaida'); //To select Blue
+  }).prop('selected', true);
+});
